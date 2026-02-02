a161bd678e1bc0d67199c3e3fb37068f450cd631
Louie Camacho <louiecamacho@gmail.com>
2026-02-02 15:59:16 -0600

    add components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,62 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
   // TODO
+  const [episodes] = useState(episodeList);
+  const [currentEpisode, setCurrentEpisode] = useState();
+
+  /** detailed info about the current episode */
+  const EpisodeDetails = () => {
+    if(!currentEpisode){
+      return (
+        <section id="details">
+          <h2>Details</h2>
+          <p>Select an Episode</p>
+      </section>
+      )
+    }
+    return (
+      <section id="details">
+        <h2>{currentEpisode.title}</h2>
+        <p>{currentEpisode.description}</p>
+        <button>Watch episode {currentEpisode.id} now</button>
+      </section>
+    );
+  }
+  
+  /** list of episodes that the user can select from */
+  const EpisodeList = () => {
+    return (
+      
+      <section id="episodes">
+        <h2>Episodes</h2>
+
+        <ul className="episode-list">
+          {episodes.map(episode => 
+            <li key={episode.id} onClick={() => setCurrentEpisode(episode)}>
+              {episode.title}
+            </li>
+          )}
+        </ul>
+      
+      </section>
+
+    );
+  }
+
+  // pass the data along
+  return (
+    <main>
+        <header>
+          <h1>Dark Echoes</h1>
+        </header>
+        <div className="episode-container">
+          <EpisodeList />
+          <EpisodeDetails />
+        </div>
+    </main>
+        
+
+  );
 }
